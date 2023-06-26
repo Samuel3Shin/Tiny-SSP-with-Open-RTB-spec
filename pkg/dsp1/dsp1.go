800803be953da25e8ec62118036980ec2ce6feac
Samuel3Shin <samuel3.shin@gmail.com>
2023-06-25 21:08:37 -0700

    added more bids to generate in dsp servers

--- pkg/dsp1/dsp1.go
+++ pkg/dsp1/dsp1.go
@@ -2,6 +2,7 @@ package dsp1
 
 import (
 	"encoding/json"
+	"fmt"
 	"math/rand"
 	"net/http"
 	"time"
@@ -28,21 +29,28 @@ func GetBidHandler(w http.ResponseWriter, r *http.Request) {
 func GenerateBid(bidRequest common.BidRequest) common.BidResponse {
 	cfg := common.GetConfig()
 	rand.Seed(time.Now().UnixNano())
-	bidAmount := rand.Float64() * 100
+	seatBids := []common.SeatBid{}
+
+	for i := 0; i < 3; i++ {
+		bids := []common.Bid{}
+		for j := 0; j < 3; j++ {
+			bidAmount := rand.Float64() * 100
+			bids = append(bids, common.Bid{
+				ID:    fmt.Sprintf("bid%d", j+1),
+				ImpID: bidRequest.Imp[0].ID,
+				Price: bidAmount,
+				AdID:  fmt.Sprintf("ad%d", j+1),
+				NURL:  cfg.LOGSERVER_URL,
+			})
+		}
+
+		seatBids = append(seatBids, common.SeatBid{
+			Bid: bids,
+		})
+	}
+
 	return common.BidResponse{
-		ID: bidRequest.ID,
-		SeatBid: []common.SeatBid{
-			{
-				Bid: []common.Bid{
-					{
-						ID:    "12",
-						ImpID: bidRequest.Imp[0].ID,
-						Price: bidAmount,
-						AdID:  "ad1",
-						NURL:  cfg.LOGSERVER_URL,
-					},
-				},
-			},
-		},
+		ID:      bidRequest.ID,
+		SeatBid: seatBids,
 	}
 }
